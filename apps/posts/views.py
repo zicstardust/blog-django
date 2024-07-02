6792725b3375526b1d9719b8483e71ecdb373903
Zic Stardust <git@zicstardust.com>
2024-07-02 10:41:42 -0400

    create templates e rotes

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Posts
 
 # Create your views here.
+
+def post_list(request):
+    template_name = 'post-list.html'
+    posts = Posts.objects.all()
+    context = {
+        'posts' : posts
+    }
+    return render(request, template_name, context)
--- core/urls.py
+++ core/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from apps.posts import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('apps.posts.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
